479efe577062e9a1fbd7c5dd5d0f8234b3af8d9f
mandyyrl <ruiliny@andrew.cmu.edu>
2025-09-21 02:01:37 +0000

    feat(private): add private post toggle UI

--- public/src/client/compose.js
+++ public/src/client/compose.js
@@ -8,10 +8,46 @@ define('forum/compose', ['hooks'], function (hooks) {
 		const container = $('.composer');
 
 		if (container.length) {
+			// Initialize private post toggle functionality
+			Compose.setupPrivateToggle(container);
+
 			hooks.fire('action:composer.enhance', {
 				container: container,
 			});
 		}
+	};
+
+	Compose.setupPrivateToggle = function (container) {
+		container.off('click', '.composer-private').on('click', '.composer-private', function (e) {
+			e.preventDefault();
+			const btn = $(this);
+			const icon = btn.find('i');
+			const text = btn.find('.private-text');
+
+			// Get the post UUID from the container
+			const post_uuid = container.attr('data-uuid');
+
+			// Toggle private state
+			if (btn.hasClass('active')) {
+				btn.removeClass('active');
+				icon.removeClass('fa-lock').addClass('fa-unlock');
+				text.text('Public');
+
+				// Update composer internal state if available
+				if (window.composer && window.composer.posts && window.composer.posts[post_uuid]) {
+					window.composer.posts[post_uuid].isPrivate = false;
+				}
+			} else {
+				btn.addClass('active');
+				icon.removeClass('fa-unlock').addClass('fa-lock');
+				text.text('Private');
+
+				// Update composer internal state if available
+				if (window.composer && window.composer.posts && window.composer.posts[post_uuid]) {
+					window.composer.posts[post_uuid].isPrivate = true;
+				}
+			}
+		});
 	};
 
 	return Compose;
