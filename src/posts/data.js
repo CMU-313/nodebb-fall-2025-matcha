498b44b05f43086a6d93c5ff0099b24de033613a
Sarah Cross <sarahcro@andrew.cmu.edu>
2025-10-31 17:33:20 +0000

    perf: updated to sync with bugfix + pass tests

--- src/posts/data.js
+++ src/posts/data.js
@@ -71,11 +71,11 @@ function modifyPost(post, fields) {
 		if (!fields.length || fields.includes('attachments')) {
 			post.attachments = (post.attachments || '').split(',').filter(Boolean);
 		}
+<<<<<<< HEAD
+=======
 		// Mark post as "English" if decided by translator service or if it has no info
 		post.isEnglish = post.isEnglish == 'true' || post.isEnglish === undefined;
-		// If translatedContent is undefined, default to empty string (no translation needed for English posts)
-		if (post.translatedContent === undefined) {
-			post.translatedContent = '';
-		}
+		post.translatedContent = post.translatedContent ?? post.content;
+>>>>>>> a2f3ab4643 (perf: updated to sync with bugfix + pass tests)
 	}
 }
