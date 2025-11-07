3fa959b062cc33c2f56af44dea7684b1bdab9c0e
matthewkwee_wsl <matthewkwee@icloud.com>
2025-11-07 21:48:55 +0000

    Changed API URL to deployed VM microservice

--- src/translate/index.js
+++ src/translate/index.js
@@ -9,8 +9,8 @@ const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
 	try {
-		const TRANSLATOR_API = '127.0.0.1:5000'; // TODO: Add the translator API URL
-		const response = await fetch(TRANSLATOR_API + ' /?content=' + postData.content);
+		const TRANSLATOR_API = 'http://17313-team04.s3d.cmu.edu:5000'; // TODO: Add the translator API URL
+		const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
 		const data = await response.json();
 		return [data.is_english, data.translated_content];
 	} catch (error) {
