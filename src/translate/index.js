2e6ae06f8becd6cde4ef41a7f222e2f5f2b5efa2
matthewkwee_wsl <matthewkwee@icloud.com>
2025-11-07 22:22:27 +0000

    Dummied out the API call to force the Translate button to display

--- src/translate/index.js
+++ src/translate/index.js
@@ -8,14 +8,17 @@ const translatorApi = module.exports;
 // };
 
 translatorApi.translate = async function (postData) {
+	console.log(postData.content);
 	try {
-		const TRANSLATOR_API = 'http://17313-team04.s3d.cmu.edu:5000'; // TODO: Add the translator API URL
+		const TRANSLATOR_API = '127.0.0.1:5000'; // TODO: Add the translator API URL
+		//const TRANSLATOR_API = 'http://17313-team04.s3d.cmu.edu:5000'; // TODO: Add the translator API URL
 		const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
+		
 		const data = await response.json();
 		return [data.is_english, data.translated_content];
 	} catch (error) {
 		console.error(error);
-		return [true, postData.content];
+		return [false, postData.content]; //[true, postData.content];
 	}
 };
 
