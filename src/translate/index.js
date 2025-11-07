1e5babaa58857c7be63b422b1ac9720dbc5bfb53
matthewkwee_wsl <matthewkwee@icloud.com>
2025-11-07 23:12:22 +0000

    Testing for deployment issues for translate frontent

--- src/translate/index.js
+++ src/translate/index.js
@@ -13,7 +13,7 @@ translatorApi.translate = async function (postData) {
 		const TRANSLATOR_API = '127.0.0.1:5000'; // TODO: Add the translator API URL
 		//const TRANSLATOR_API = 'http://17313-team04.s3d.cmu.edu:5000'; // TODO: Add the translator API URL
 		const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
-		
+
 		const data = await response.json();
 		return [data.is_english, data.translated_content];
 	} catch (error) {
